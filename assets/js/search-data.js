3f52596081b397c2a5af1945b413139d037084fa
laurent-barbe <laurent-barbe@users.noreply.github.com>
2025-08-13 08:41:22 +0000

    Deploying to gh-pages from @ laurent-barbe/laurent-barbe.github.io@519df183b7ecd5a03b93e67bb36bf5b3c18cdd37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Aperçu des projets.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -451,14 +451,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-ramsai",
+          title: 'RAMSAI',
+          description: "Robotized Additive Manufacturing for Silicone assisted by Artificial Intelligence",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-bifasi",
+          title: 'BIFASI',
+          description: "Buse Intelligente pour la Fabrication Additive SIlicone",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
